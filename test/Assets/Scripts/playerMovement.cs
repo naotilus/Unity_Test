37f1872eeb04e0ea6ba6435d92e3d076a4132a5d
ole1711 <42774821+ole1711@users.noreply.github.com>
2018-09-01 00:05:23 +0200

    imported assets, created movement script, created reset script

---
+++ test/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Vector2 speed = new Vector2(25, 25);
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputX = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(speed.x * inputX, 0, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
---
+++ test/Assets/Scripts/reset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reset : MonoBehaviour {
+// Update is called once per frame
+	void Update () {
+
+
+        Vector3 start = new Vector3(0, 0, 0);
+
+        Vector3 posY = transform.position;
+
+        if(posY.y <= -10)
+        {
+            transform.position = start;
+            Debug.Log("Penis. Errigiert.");
+
+        }
+	}
+}
